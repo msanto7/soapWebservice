839eec8a9156206738028eb9bc29b346635b9f15
Mike Santoro <mike.santoro42@gmail.com>
2019-06-27 19:13:55 -0400

    initial commit

---
+++ mikesWebService.asmx/mikesWebService.asmx/WebDev1Service.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebDev1Service.asmx.cs" Class="mikesWebService.asmx.WebDev1Service" %>
---
+++ mikesWebService.asmx/mikesWebService.asmx/WebDev1Service.asmx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace mikesWebService.asmx
+{
+    /// <summary>
+    /// Summary description for WebDev1Service
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+
+
+
+    public class WebDev1Service : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string BikesStudioService()  
+        {
+            return "From BikesOnRails to BikesStudio. SOAP Example.";
+
+        }
+
+
+    } // Ends Web Service Class 
+
+}
