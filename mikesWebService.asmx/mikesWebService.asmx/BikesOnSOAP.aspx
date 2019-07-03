5003544d937a0919b74a9fbd70294d6ea792a168
Mike Santoro <mike.santoro42@gmail.com>
2019-07-03 13:59:53 -0400

    added webform page to make request

---
+++ mikesWebService.asmx/mikesWebService.asmx/BikesOnSOAP.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BikesOnSOAP.aspx.cs" Inherits="mikesWebService.asmx.WebForm1" %>
+
+<!DOCTYPE html>
+  
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head runat="server">  
+    <title></title>  
+</head>  
+<body>  
+    <form id="form1" runat="server">  
+        <div>  
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" style="height: 26px" /><asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>  
+        </div>  
+    </form>  
+</body>  
+</html>  
---
+++ mikesWebService.asmx/mikesWebService.asmx/BikesOnSOAP.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mikesWebService.asmx
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebDev1Service webService = new WebDev1Service();
+            List<int> lstIntegers = new List<int> { 5, 6, 7 };
+
+            Label1.Text = "Output of WebService: " + webService.DesiredAmountOfBikes(lstIntegers).ToString();
+
+        }
+
+
+    }
+}
